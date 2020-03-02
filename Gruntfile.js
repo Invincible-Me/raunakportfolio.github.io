6f6eb253920a8be6487c959736dbdc19bf6006ae
Raunak Singh <invincibleme.404@gmail.com>
2020-03-03 01:03:55 +0530

    navbar

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         concat: {
             css: {
-                src : ['css/sass/main.scss', 'css/sass/theme.scss'],
+                src : ['css/main.css', 'css/noscript.css', 'css/scss/**/*.scss'],
                 dest: 'build/css/styles.css',
             },
         },
@@ -13,20 +13,31 @@ module.exports = function(grunt) {
                 dest: 'build/js/main.min.js',
             }
         },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    cwd: 'images/',
+                    expand: true,
+                    src: ['**/*.{png,jpg,svg}'],
+                    dest: 'build/images'
+                  }]
+            }
+        },
         watch: {
             js: {
                 files: ['js/**/*.js'],
                 tasks: ['uglify:js'],
             },
             css: {
-                files: ['css/**/*.scss'],
+                files: ['css/**/*.scss', 'css/*.css'],
                 tasks: ['concat:css'],
             },
         },
     });
 
 grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-imagemin');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.registerTask('default', ['concat', 'watch', 'uglify']);
+grunt.registerTask('default', ['concat', 'watch', 'uglify', 'imagemin']);
 };
